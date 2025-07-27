ee3398439a58fb2134bc8f8f30105b8376ede4b0
Sharvesh Sivagnanam <sharveshprojects@gmail.com>
2025-07-28 04:23:45 +0530

    Implemented basic websocket with socket io

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,12 +1,14 @@
-import { WebSocketGateway, WebSocketServer,SubscribeMessage,MessageBody } from '@nestjs/websockets';
-
-@WebSocketGateway(8020,{ namespace: 'chat' , cors: true , transports: ['websocket'] })
+import { WebSocketGateway, WebSocketServer,SubscribeMessage,MessageBody,ConnectedSocket } from '@nestjs/websockets';
+import { Socket } from 'net';
+@WebSocketGateway(8020,{ namespace: 'chat' , cors: true , transports: ['websocket','polling'] })
 export class ChatGateway {
   // Gateway logic will go here
 
 @SubscribeMessage('events')
-handleEvent(@MessageBody() data: string): string {
+handleEvent(@MessageBody() data: string,@ConnectedSocket() client: Socket,): string {
     console.log(data)
+    client.emit('events', data);
+  
   return data;
 }
 
